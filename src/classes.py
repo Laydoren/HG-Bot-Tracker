6ae64f85b736d7922bada33faa113a8961691401
Lay <dogfish520@gmail.com>
2026-05-28 22:14:51 +0800

    Adding core classes Habit, Completion, Goal

---
+++ src/classes.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+class Habit:
+    def __init__(self, id, name, frequency, created_at):
+        self.id = id
+        self.name = name
+        self.frequency = frequency
+        self.created_at = created_at
+
+
+class Completion:
+    def __init__(self, habit_id, completed_at):
+        self.habit_id = habit_id
+        self.completed_at = completed_at
+
+
+class Goal:
+    def __init__(self, id, title, target, current, deadline):
+        self.id = id
+        self.title = title
+        self.target = target
+        self.current = current
+        self.deadline = deadline
+
+
+
+
