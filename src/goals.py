62afc08d81cbbc5bda9b381fbc7eed7de59504ca
pola0104 <pola0111.0444@gmail.com>
2026-06-03 17:40:50 +0800

    Refactor goal status logic into separate function

--- src/goals.py
+++ src/goals.py
@@ -1,13 +1,18 @@
 from datetime import date
 from src.classes import Goal
 
-def update_goal_status(goal, today=None):
+def get_goal_status(goal, today=None):
     if today is None:
         today = date.today()
-    if  goal.current >= goal.target:
-        goal.status_completion = "Done"
+    if goal.current >= goal.target:
+        return "Done"
     elif today > goal.deadline:
-        goal.status_completion= "Not completed"
+        return "Not completed"
     else:
-        goal.status_completion= "In process"
+        return "In process"
+
+def update_goal_status(goal, today=None):
+    goal.status_completion = get_goal_status(goal, today)
+
+
 
