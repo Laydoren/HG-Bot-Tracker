6e1f964a5027c305d5f31de197893cfd66d53073
pola0104 <pola0111.0444@gmail.com>
2026-06-03 16:39:49 +0800

    Add function to update goal completion status

---
+++ src/goals.py
@@ -0,0 +1,13 @@
+from datetime import date
+from classes import Goal
+
+def update_goal_status(goal, today=None):
+    if today is None:
+        today = date.today()
+    if  goal.current >= goal.target:
+        goal.status_completion = "Done"
+    elif today > goal.deadline:
+        goal.status_completion= "Not completed"
+    else:
+        goal.status_completion= "In process"
+
